df7a367b57f2bc0f45ff00fb477dd474d9d5ca51
migmolrod <migmolrod@fastmail.es>
2023-07-01 21:39:50 +0200

    fixed error beer order line had no upc. closes #6

--- services/mssc-beer-order-service/src/main/java/guru/sfg/beer/order/service/domain/BeerOrderLine.java
+++ services/mssc-beer-order-service/src/main/java/guru/sfg/beer/order/service/domain/BeerOrderLine.java
@@ -38,16 +38,18 @@ public class BeerOrderLine extends BaseEntity {
   @ManyToOne
   private BeerOrder beerOrder;
   private UUID beerId;
+  private String upc;
   private Integer orderQuantity = 0;
   private Integer quantityAllocated = 0;
 
   @Builder
   public BeerOrderLine(UUID id, Long version, Timestamp createdDate, Timestamp lastModifiedDate,
-                       BeerOrder beerOrder, UUID beerId, Integer orderQuantity,
+                       BeerOrder beerOrder, UUID beerId, String upc, Integer orderQuantity,
                        Integer quantityAllocated) {
     super(id, version, createdDate, lastModifiedDate);
     this.beerOrder = beerOrder;
     this.beerId = beerId;
+    this.upc = upc;
     this.orderQuantity = orderQuantity;
     this.quantityAllocated = quantityAllocated;
   }
