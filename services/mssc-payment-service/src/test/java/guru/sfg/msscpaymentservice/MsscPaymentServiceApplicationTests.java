23248f2b1e928e2aa9b0880090e161fc83034a88
migmolrod <migmolrod@fastmail.es>
2023-07-12 20:25:25 +0200

    created new payment service
    essentially, it will manage a state machine
    and all events and actions to change states

---
+++ services/mssc-payment-service/src/test/java/guru/sfg/msscpaymentservice/MsscPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.sfg.msscpaymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscPaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
