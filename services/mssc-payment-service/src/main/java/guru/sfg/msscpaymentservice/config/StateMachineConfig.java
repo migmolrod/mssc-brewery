fe9551631cc58543ff73ded6f03a6efcbde94e73
migmolrod <migmolrod@fastmail.es>
2023-07-12 21:27:36 +0200

    added state mac hine configuration
    also added very simple state machine tests

--- services/mssc-payment-service/src/main/java/guru/sfg/msscpaymentservice/config/StateMachineConfig.java
+++ services/mssc-payment-service/src/main/java/guru/sfg/msscpaymentservice/config/StateMachineConfig.java
@@ -7,20 +7,39 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
 
 import java.util.EnumSet;
 
 @Slf4j
-@Configuration
 @EnableStateMachineFactory
+@Configuration
 public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentState, PaymentEvent> {
 
   @Override
   public void configure(StateMachineStateConfigurer<PaymentState, PaymentEvent> states) throws Exception {
-    states.withStates().initial(PaymentState.NEW)
+    states.withStates()
+        .initial(PaymentState.NEW)
         .states(EnumSet.allOf(PaymentState.class))
         .end(PaymentState.AUTH)
         .end(PaymentState.PRE_AUTH_ERROR)
         .end(PaymentState.AUTH_ERROR);
+  }
+
+  @Override
+  public void configure(StateMachineTransitionConfigurer<PaymentState, PaymentEvent> transitions) throws Exception {
+    transitions
+        .withExternal().source(PaymentState.NEW).target(PaymentState.NEW).event(PaymentEvent.PRE_AUTHORIZE)
+        .and()
+        .withExternal().source(PaymentState.NEW).target(PaymentState.PRE_AUTH).event(PaymentEvent.APPROVE_PRE_AUTHORIZE)
+        .and()
+        .withExternal().source(PaymentState.NEW).target(PaymentState.PRE_AUTH_ERROR).event(PaymentEvent.DECLINE_PRE_AUTHORIZE)
+        .and()
+        .withExternal().source(PaymentState.PRE_AUTH).target(PaymentState.PRE_AUTH).event(PaymentEvent.AUTHORIZE)
+        .and()
+        .withExternal().source(PaymentState.PRE_AUTH).target(PaymentState.AUTH).event(PaymentEvent.APPROVE_AUTHORIZE)
+        .and()
+        .withExternal().source(PaymentState.PRE_AUTH).target(PaymentState.AUTH_ERROR).event(PaymentEvent.DECLINE_AUTHORIZE)
+    ;
   }
 }
