c62459548312eeed9f2c65e39137424db42c3b29
migmolrod <migmolrod@fastmail.es>
2023-07-19 19:15:54 +0200

    configured validation order events

--- services/mssc-beer-order-service/src/main/java/guru/sfg/beer/order/service/state/BeerOrderStateMachineConfig.java
+++ services/mssc-beer-order-service/src/main/java/guru/sfg/beer/order/service/state/BeerOrderStateMachineConfig.java
@@ -6,12 +6,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
 
 import java.util.EnumSet;
 
 @EnableStateMachineFactory
 @Configuration
-public class BeerOrderStateMachineConfig extends StateMachineConfigurerAdapter<BeerOrderStatusEnum, BeerOrderEventEnum> {
+public class BeerOrderStateMachineConfig extends StateMachineConfigurerAdapter<BeerOrderStatusEnum,
+    BeerOrderEventEnum> {
 
   @Override
   public void configure(StateMachineStateConfigurer<BeerOrderStatusEnum, BeerOrderEventEnum> states) throws Exception {
@@ -25,5 +27,16 @@ public class BeerOrderStateMachineConfig extends StateMachineConfigurerAdapter<B
         .end(BeerOrderStatusEnum.ALLOCATION_EXCEPTION);
   }
 
-
+  @Override
+  public void configure(StateMachineTransitionConfigurer<BeerOrderStatusEnum, BeerOrderEventEnum> transitions) throws Exception {
+    transitions
+        .withExternal()
+        .source(BeerOrderStatusEnum.NEW).target(BeerOrderStatusEnum.NEW).event(BeerOrderEventEnum.VALIDATE_ORDER)
+        .and()
+        .withExternal()
+        .source(BeerOrderStatusEnum.NEW).target(BeerOrderStatusEnum.VALIDATED).event(BeerOrderEventEnum.VALIDATION_PASSED)
+        .and()
+        .withExternal()
+        .source(BeerOrderStatusEnum.NEW).target(BeerOrderStatusEnum.VALIDATION_EXCEPTION).event(BeerOrderEventEnum.VALIDATION_FAILED);
+  }
 }
