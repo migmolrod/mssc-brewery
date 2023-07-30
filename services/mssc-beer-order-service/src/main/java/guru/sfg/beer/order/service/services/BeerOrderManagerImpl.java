513100cb26bd1f247f0841fd53e713af773c3587
migmolrod <migmolrod@fastmail.es>
2023-07-30 17:23:58 +0200

    changelog:
    - added order manager service
    - added order state change interceptor

---
+++ services/mssc-beer-order-service/src/main/java/guru/sfg/beer/order/service/services/BeerOrderManagerImpl.java
@@ -0,0 +1,66 @@
+package guru.sfg.beer.order.service.services;
+
+import guru.sfg.beer.order.service.domain.BeerOrder;
+import guru.sfg.beer.order.service.domain.BeerOrderEventEnum;
+import guru.sfg.beer.order.service.domain.BeerOrderStatusEnum;
+import guru.sfg.beer.order.service.repositories.BeerOrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.config.StateMachineFactory;
+import org.springframework.statemachine.support.DefaultStateMachineContext;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class BeerOrderManagerImpl implements BeerOrderManager {
+
+  public static final String BEER_ORDER_ID_HEADER = "order_id";
+
+  private final StateMachineFactory<BeerOrderStatusEnum, BeerOrderEventEnum> stateMachineFactory;
+  private final BeerOrderRepository beerOrderRepository;
+  private final BeerOrderStateChangeInterceptor beerOrderStateChangeInterceptor;
+
+  @Override
+  @Transactional
+  public BeerOrder newBeerOrder(BeerOrder beerOrder) {
+    beerOrder.setId(null);
+    beerOrder.setOrderStatus(BeerOrderStatusEnum.NEW);
+
+    BeerOrder savedBeerOrder = beerOrderRepository.save(beerOrder);
+    sendBeerOrderEvent(beerOrder, BeerOrderEventEnum.VALIDATE_ORDER);
+
+    return savedBeerOrder;
+  }
+
+  private void sendBeerOrderEvent(BeerOrder beerOrder, BeerOrderEventEnum event) {
+    StateMachine<BeerOrderStatusEnum, BeerOrderEventEnum> stateMachine = build(beerOrder);
+
+    Message<BeerOrderEventEnum> message = MessageBuilder.withPayload(event)
+        .setHeader(BEER_ORDER_ID_HEADER, beerOrder.getId())
+        .build();
+
+    stateMachine.sendEvent(message);
+  }
+
+  private StateMachine<BeerOrderStatusEnum, BeerOrderEventEnum> build(BeerOrder beerOrder) {
+    StateMachine<BeerOrderStatusEnum, BeerOrderEventEnum> stateMachine =
+        stateMachineFactory.getStateMachine(beerOrder.getId());
+
+    stateMachine.stop();
+
+    stateMachine.getStateMachineAccessor()
+        .doWithAllRegions(stateMachineAccessor -> {
+          stateMachineAccessor.addStateMachineInterceptor(beerOrderStateChangeInterceptor);
+          stateMachineAccessor.resetStateMachine(
+              new DefaultStateMachineContext<>(beerOrder.getOrderStatus(), null, null, null)
+          );
+        });
+
+    stateMachine.start();
+
+    return stateMachine;
+  }
+}
---
+++ services/mssc-beer-order-service/src/main/java/guru/sfg/beer/order/service/services/BeerOrderStateChangeInterceptor.java
@@ -0,0 +1,36 @@
+package guru.sfg.beer.order.service.services;
+
+import guru.sfg.beer.order.service.domain.BeerOrder;
+import guru.sfg.beer.order.service.domain.BeerOrderEventEnum;
+import guru.sfg.beer.order.service.domain.BeerOrderStatusEnum;
+import guru.sfg.beer.order.service.repositories.BeerOrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.Message;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.state.State;
+import org.springframework.statemachine.support.StateMachineInterceptorAdapter;
+import org.springframework.statemachine.transition.Transition;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Component
+public class BeerOrderStateChangeInterceptor extends StateMachineInterceptorAdapter<BeerOrderStatusEnum,
+    BeerOrderEventEnum> {
+
+  private final BeerOrderRepository beerOrderRepository;
+
+  @Override
+  public void preStateChange(State<BeerOrderStatusEnum, BeerOrderEventEnum> state,
+                             Message<BeerOrderEventEnum> message,
+                             Transition<BeerOrderStatusEnum, BeerOrderEventEnum> transition,
+                             StateMachine<BeerOrderStatusEnum, BeerOrderEventEnum> stateMachine) {
+    Optional.ofNullable(message).flatMap(msg -> Optional.ofNullable((UUID) message.getHeaders().getOrDefault(BeerOrderManagerImpl.BEER_ORDER_ID_HEADER, -1L))).ifPresent(orderId -> {
+      BeerOrder order = this.beerOrderRepository.getOne(orderId);
+      order.setOrderStatus(state.getId());
+      beerOrderRepository.save(order);
+    });
+  }
+}
