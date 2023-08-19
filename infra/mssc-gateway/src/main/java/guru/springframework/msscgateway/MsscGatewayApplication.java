79a31453e02a726386e1f0700dc9fc355f8bc5c6
migmolrod <migmolrod@fastmail.es>
2023-08-19 15:31:04 +0200

    incremental #34
    added new module for gateway, with a similar
    structure as services (with an aggregator pom
    called "infra" that inherits from the parent
    pom)

---
+++ infra/mssc-gateway/src/main/java/guru/springframework/msscgateway/MsscGatewayApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.msscgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscGatewayApplication.class, args);
+	}
+
+}
---
+++ infra/mssc-gateway/src/test/java/guru/springframework/msscgateway/MsscGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.msscgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
