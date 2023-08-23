6c357b7d2c3936dcdf01a0c2bad83f0ef76618bd
migmolrod <migmolrod@fastmail.es>
2023-08-23 18:14:20 +0200

    incremental #38
    added eureka client to all microservices
    
    also added a missing piece in circleci config.
    now, changes in services/pom.xml will trigger
    a build on every microservice that uses that
    pom

---
+++ services/mssc-beer-service/src/main/java/guru/sfg/msscbeerservice/config/LocalDiscovery.java
@@ -0,0 +1,11 @@
+package guru.sfg.msscbeerservice.config;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@EnableDiscoveryClient
+@Configuration
+@Profile("local")
+public class LocalDiscovery {
+}
